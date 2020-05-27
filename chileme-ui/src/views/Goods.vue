ed551fc32dbbe829dd60c5252e5b68e5e45aa5d1
apondon <apondon@126.com>
2020-05-27 16:03:11 +0800

    52756

--- chileme-ui/src/views/Goods.vue
+++ chileme-ui/src/views/Goods.vue
@@ -14,7 +14,7 @@
                     div.usuallyGoods
                         h1.goodsTitle 常用商品
                         div.goodsContent
-                            div.uItems(v-for='o in uGoods',:key='o.id',@click='addToCarts(o)')
+                            div.uItems(v-for='o in uGoods',:key='o.goodsId',@click='addToCarts(o)')
                                 span.uItemsName {{o.goodname}}
                                 span.uItemsPrice  {{`￥${o.price}元`}}
                     //- 商品分类
@@ -23,7 +23,7 @@
                             el-tab-pane(label="热菜" )
                                 div
                                     div.sGoodsContent
-                                        div.goodsItem(v-for='item in hotFoods',:key='item.id',@click='addToCarts(item)')
+                                        div.goodsItem(v-for='item in hotFoods',:key='item.goodsId',@click='addToCarts(item)')
                                             div.goodsPic 
                                             div.goodsDes 
                                                 div.goodsDesName {{item.goodname}}
@@ -32,7 +32,7 @@
                             el-tab-pane(label="凉菜" )
                                 div
                                     div.sGoodsContent
-                                        div.goodsItem(v-for='item in coldFoods',:key='item.id',@click='addToCarts(item)')
+                                        div.goodsItem(v-for='item in coldFoods',:key='item.goodsId',@click='addToCarts(item)')
                                             div.goodsPic 
                                             div.goodsDes 
                                                 div.goodsDesName {{item.goodname}}
@@ -40,7 +40,7 @@
                             el-tab-pane(label="主食" )
                                 div
                                     div.sGoodsContent
-                                        div.goodsItem(v-for='item in stampFood',:key='item.id',@click='addToCarts(item)')
+                                        div.goodsItem(v-for='item in stampFood',:key='item.goodsId',@click='addToCarts(item)')
                                             div.goodsPic 
                                             div.goodsDes 
                                                 div.goodsDesName {{item.goodname}}
@@ -48,7 +48,7 @@
                             el-tab-pane(label="饮料" )
                                 div
                                     div.sGoodsContent
-                                        div.goodsItem(v-for='item in drinks',:key='item.id',@click='addToCarts(item)')
+                                        div.goodsItem(v-for='item in drinks',:key='item.goodsId',@click='addToCarts(item)')
                                             div.goodsPic 
                                             div.goodsDes 
                                                 div.goodsDesName {{item.goodname}}
@@ -63,44 +63,13 @@ export default {
             activeName:'', // 购物车和订单的切换
             menuActiveName:'', //商品分类的切换
             // 常用商品
-            uGoods:[
-                {id:1,goodname:'第一道菜',price:'10'},
-                {id:2,goodname:'第二道菜',price:'11'},
-                {id:3,goodname:'第三道菜',price:'12'},
-                {id:4,goodname:'第四道菜',price:'13'},
-                {id:5,goodname:'第五道菜',price:'14'},
-                {id:6,goodname:'第六道菜',price:'15'},
-                {id:7,goodname:'第七道菜',price:'16'},
-                {id:8,goodname:'第八道菜',price:'21'},
-                {id:9,goodname:'第九道菜',price:'25'},
-                {id:10,goodname:'第十道菜',price:'35'},
-            ],
-            // 商品分类
-            sGoods:[
-                {id:1,name:'汉堡',code:'berger'},
-                {id:2,name:'主食',code:'staple'},
-                {id:3,name:'饮料',code:'drink'}
-            ],
+            uGoods:[],
+            
             // 分类页签中的商品数据
-            hotFoods:[
-              {id:'ht001',goodname:'hot1',price:11,rate:3},  
-              {id:'ht002',goodname:'hot2',price:11.2,rate:3},
-              {id:'ht003',goodname:'hot3',price:16,rate:3}
-            ],
-            coldFoods:[
-                {id:'cd001',goodname:'cold1',price:7,rate:3},
-                {id:'cd002',goodname:'cold2',price:8,rate:3},
-                {id:'cd003',goodname:'cold3',price:9,rate:3}
-            ],
-            stampFood:[
-                {id:'st001',goodname:'rice',price:3,rate:3},
-                {id:'st002',goodname:'noodles',price:6,rate:3},
-            ],
-            drinks:[
-                {id:'dk001',goodname:'beer',price:5,rate:3},
-                {id:'dk002',goodname:'soft',price:3.5,rate:3},
-                {id:'dk003',goodname:'juice',price:10,rate:3},
-            ],
+            hotFoods:[],
+            coldFoods:[],
+            stampFood:[],
+            drinks:[],
             cartsList:[], //购物车数据
             ordersList:[
                 {orderNo:'ODR00001',date:'2020-05-20 15:18',price:100,id:'odr01'},
@@ -120,16 +89,18 @@ export default {
         this.getOrderData() // 获取订单数据
     },
     methods:{
+        // 向购物车中添加商品
         addToCarts(item){
             this.Axios({
                 method:'POST',
                 url:'/api/carts/addGoods',
                 data:{
-                    goodsId:1587949603505
+                    goodsId:item.goodsId
                 }
             })
             .then(res => {  // 对请求成功的结果进行处理   res成功的结果
                 console.log(res)
+                this.getCartsData()
             })
             .catch(function(err){ // 对请求失败的结果进行处理   err失败的结果
                 console.log(err)
@@ -139,32 +110,32 @@ export default {
         getFoodsData(){
             this.Axios({
                 method:'GET', //请求方式
-                url:'', // 请求地址
-                data:{}, // 请求携带的参数，若该请求不需要携带参数，则可以忽略该属性            
+                url:'/api/user/getGoodsList', // 请求地址
             }).then(res => { //请求成功的回调函数  res请求返回的结果
-                console.log(res)
+                console.log(res) 
+                // 遍历数据
+                for(let i=0;i<res.data.data.length;i++){
+                    // uGoods 常用商品 isRecommend : true
+                    if(res.data.data[i].isRecommend) this.uGoods.push(res.data.data[i])
+                    // hotFoods 热菜  type: heat
+                    if(res.data.data[i].type === 'heat') this.hotFoods.push(res.data.data[i])
+                    // coldFoods 凉菜  type: cool
+                    if(res.data.data[i].type === 'cool') this.coldFoods.push(res.data.data[i])
+                    // stampFood 主食 type: staple
+                    if(res.data.data[i].type === 'staple') this.stampFood.push(res.data.data[i])
+                    // drinks 饮料  type: soup
+                    if(res.data.data[i].type === 'soup') this.drinks.push(res.data.data[i])
+                }
             }).catch(err => { // 请求失败的回调函数   err请求失败的返回结果
                 console.log(err)
             })
         },
-        // 向购物车中添加商品
-        addGoods(){
-            this.Axios({
-                method:'GET', //请求方式
-                url:'', // 请求地址
-                data:{}, // 请求携带的参数，若该请求不需要携带参数，则可以忽略该属性            
-            }).then(res => { //请求成功的回调函数  res请求返回的结果
-                console.log(res)
-            }).catch(err => { // 请求失败的回调函数   err请求失败的返回结果
-                console.log(err)
-            })
-        },
+        
         // 查询购物车中商品数据
         getCartsData(){
             this.Axios({
                 method:'GET', //请求方式
-                url:'', // 请求地址
-                data:{}, // 请求携带的参数，若该请求不需要携带参数，则可以忽略该属性            
+                url:'/api/carts/queryCartsData', // 请求地址
             }).then(res => { //请求成功的回调函数  res请求返回的结果
                 console.log(res)
             }).catch(err => { // 请求失败的回调函数   err请求失败的返回结果

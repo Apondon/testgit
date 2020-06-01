5e91512b9a1b57f27413ecd601bfeda50744f8a3
apondon <apondon@126.com>
2020-06-01 16:32:05 +0800

    60156

--- chileme-sev/controller/order_controller.js
+++ chileme-sev/controller/order_controller.js
@@ -1,7 +1,25 @@
 const Order = require('../model/order_schema')
+const Goods = require('../model/goods_schema')
 
 // 新增订单
 const addOrder = async ctx => {
+    // 获取商品id
+    let req = ctx.request.body
+    console.log(req)
+    // 定义一个变量来收集查询出来的商品数据
+    let collectGoods = []
+    // 根据商品id去库中查询商品信息
+    await Goods.find({goodsId:{$in:req.ids}})
+    .then(res => { //查询成功
+        console.log(res)
+        collectGoods = res
+    }).catch(err => { //查询失败
+        console.log(err)
+        ctx.body = {
+            success : false,
+            msg:'查询商品数据失败'
+        }
+    })
     ctx.body='新增成功'
 }
 // 查看订单
--- chileme-ui/src/components/Carts.vue
+++ chileme-ui/src/components/Carts.vue
@@ -37,10 +37,20 @@ export default {
         },
         // 创建订单
         addOrder(){
+            // 定义一个数组来存放id
+            let arr = []
+            // 遍历商品数据
+            for(let i=0;i<this.cartsList.length;i++){
+                // 将每个元素的goodsId存入新数组
+                arr.push(this.cartsList[i].goodsId)
+            }
+            console.log(arr)
             this.Axios({
                 method:'POST', //请求方式
                 url:'/api/order/addOrder', // 请求地址
-                data:{}, // 请求携带的参数，若该请求不需要携带参数，则可以忽略该属性            
+                data:{
+                    ids:arr
+                }, // 请求携带的参数，若该请求不需要携带参数，则可以忽略该属性            
             }).then(res => { //请求成功的回调函数  res请求返回的结果
                 console.log(res)
             }).catch(err => { // 请求失败的回调函数   err请求失败的返回结果
